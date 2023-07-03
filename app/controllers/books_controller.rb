9f4feaf19798ee87cc8910a2ddb532c6e47da8a9
EC2 Default User <ec2-user@ip-172-31-23-99.ap-northeast-1.compute.internal>
2023-07-03 07:04:23 +0000

    commit4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ before_action :is_matching_login_user, only: [:edit]
   def show
     @book = Book.find(params[:id])
     @book_new = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
